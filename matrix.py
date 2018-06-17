a968526d6e8d6ed50333b4b2b5407debe610e9ce
C <thefiercerabbit@gmail.com>
2018-06-17 11:32:31 +0100

    reading matrix from file

--- matrix.py
+++ matrix.py
@@ -1,33 +1,49 @@
 import random
 
 class Matrix:
+    def __init__(self):
+        self.n = 0           # dimension
+        self.m = None        # our matrix
 
-   def __init__(self):
-      self.n = 0       # dimension
-      self.m = None    # our matrix
+    def createMatrix(self,dim):
+        self.n = dim
+        self.m = []
 
-   def createMatrix(self,dim):
-      self.n = dim
-      self.m = []
+        for i in range(self.n):
+            self.m.append([])
+            for j in range(i):
+                v = 0
+                if random.randint(0,5) == 0:
+                    v = 1
+                self.m[i].append(v)
+            self.m[i].append(1)
 
-      for i in range(self.n):
-         self.m.append([])
-         for j in range(i):
-            v = 0
-            if random.randint(0,5) == 0:
-               v = 1
-            self.m[i].append(v)
-         self.m[i].append(1)
+    def readMatrix(self,file):
+        self.m = []
+        i = -1
+        with open(file) as f:
+            lines = f.readlines()
+            for l in lines:
+                i+=1
+                if len(l.strip()) == 0:
+                    self.n = i
+                    break
+                self.m.append([])
+                print(l.strip())
+                self.m[i] = l.strip(", ").split()
+                if len(self.m[i]) != i+1:
+                    print("WARNING! Matrix file is corrupted.")
+                print(i,"--->",self.m[i])
+        print("n =",self.n)
 
+    def print(self):
+        for i in range(self.n):
+            for j in range(i+1):
+                print("",self.get(i,j),end="")
+            print()
 
-   def print(self):
-      for i in range(self.n):
-         for j in range(i+1):
-            print("",self.get(i,j),end="")
-         print()
+    def get(self,i,j):
+        return self.m[i][j]
 
-   def get(self,i,j):
-      return self.m[i][j]
-
-   def getdimension(self):
-      return self.n
+    def getdimension(self):
+        return self.n
--- solver.py
+++ solver.py
@@ -3,94 +3,98 @@ import sys
 from matrix import Matrix
 
 class BruteForcer:
-   def __init__(self,dim,mat):
-       self.dim=dim
-       self.instance=mat
-       self.solution=[]
-       self.best_solution=[]
-       self.current=0
-       self.max_color=0
-       self.best_bound=math.ceil(math.log(dim,2))+1
-       for i in range(dim):
-          self.solution.append(0)
-          self.best_solution.append(0)
+    def __init__(self,mat):
+        self.dim=mat.getdimension()
+        self.instance=mat
+        self.solution=[]
+        self.best_solution=[]
+        self.current=0
+        self.max_color=0
+        self.best_bound=math.ceil(math.log(self.dim,2))+1
+        for i in range(self.dim):
+            self.solution.append(0)
+            self.best_solution.append(0)
 
-   def check_solution(self):
-      checker=[]
-      supports=[]
+    def check_solution(self):
+        checker=[]
+        supports=[]
 
-      for i in range(self.current):
-         checker.append([])
-         supports.append([])
-         for j in range(i+1):
-            checker[i].append([])
-            supports[i].append([])
+        for i in range(self.current):
+            checker.append([])
+            supports.append([])
+            for j in range(i+1):
+                checker[i].append([])
+                supports[i].append([])
 
-      i=self.current-1
-      for j in range(i+1):
-         if self.instance.get(i,j):
-            for k in range(j,self.current):
-               c=self.solution[k]
-               if c not in checker[i][j]:
-                  checker[i][j].append(c)
-                  supports[i][j].append(c)
-               elif c in supports[i][j]:
-                  supports[i][j].remove(c)
-
-
-      for j in range(i+1):
-         if self.instance.get(i,j) and not supports[i][j]:
-            return False
-
-      return True
+        i=self.current-1
+        for j in range(i+1):
+            if self.instance.get(i,j):
+                for k in range(j,self.current):
+                    c=self.solution[k]
+                    if c not in checker[i][j]:
+                        checker[i][j].append(c)
+                        supports[i][j].append(c)
+                    elif c in supports[i][j]:
+                        supports[i][j].remove(c)
 
 
-   def solve(self):
-      if not self.check_solution():
-         return
+        for j in range(i+1):
+            if self.instance.get(i,j) and not supports[i][j]:
+                return False
 
-      if self.current==self.dim:
-         for i in range(self.dim):
-            self.best_solution[i]=self.solution[i]
-         self.best_bound=len(set(self.solution))
-         print("Found: ",end="")
-         self.print()
-         print("> New bound to beat:",self.best_bound)
-         return
+        return True
 
-      for c in range(1,self.best_bound+1):
-         self.solution[self.current]=c
-         self.current+=1
-         old_max=self.max_color
-         if c>self.max_color:
-            self.max_color=c
 
-         if self.max_color<self.best_bound:
-            self.solve()
+    def solve(self):
+        if not self.check_solution():
+            return
 
-         self.max_color=old_max
-         self.current-=1
+        if self.current==self.dim:
+            for i in range(self.dim):
+                self.best_solution[i]=self.solution[i]
+            self.best_bound=len(set(self.solution))
+            print("Found: ",end="")
+            self.print()
+            print("> New bound to beat:",self.best_bound)
+            return
 
-         if c>=self.best_bound:
-            break
+        for c in range(1,self.best_bound+1):
+            self.solution[self.current]=c
+            self.current+=1
+            old_max=self.max_color
+            if c>self.max_color:
+                self.max_color=c
 
-   def print_current(self):
-      for i in range(self.dim):
-         print("",self.solution[i],end="")
-      print()
-   def print(self):
-      for i in range(self.dim):
-         print("",self.best_solution[i],end="")
-      print()
+            if self.max_color<self.best_bound:
+                self.solve()
+
+            self.max_color=old_max
+            self.current-=1
+
+            if c>=self.best_bound:
+                break
+
+    def print_current(self):
+        for i in range(self.dim):
+            print("",self.solution[i],end="")
+        print()
+    def print(self):
+        for i in range(self.dim):
+            print("",self.best_solution[i],end="")
+        print()
 
 if __name__ == "__main__":
-    dim=20
-    if len(sys.argv)>1:
-        dim = int(sys.argv[1])
-    m = Matrix()
-    m.createMatrix(dim)
-    s=BruteForcer(dim,m)
-    m.print()
-    s.solve()
-    print("Best found: ",end="")
-    s.print()
+     m = Matrix()
+
+     dim=20
+     if len(sys.argv)>1:
+          dim = int(sys.argv[1])
+     if dim <= 0:
+          m.readMatrix(sys.argv[2])
+     else:
+          m.createMatrix(dim)
+     s=BruteForcer(m)
+     m.print()
+     s.solve()
+     print("Best found: ",end="")
+     s.print()
